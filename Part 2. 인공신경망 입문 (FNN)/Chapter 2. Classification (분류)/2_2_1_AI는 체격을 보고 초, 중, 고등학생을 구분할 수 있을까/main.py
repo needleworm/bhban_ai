44f44ee5b842ea9bb4170d86dc99a13440d1a799
needleworm <bhban@kakao.com>
2020-08-19 19:02:44 +0900

    필요없는 부분 삭제

--- Part 2. 인공신경망 입문 (FNN)/Chapter 2. Classification (분류)/2_2_1_AI는 체격을 보고 초, 중, 고등학생을 구분할 수 있을까/main.py
+++ Part 2. 인공신경망 입문 (FNN)/Chapter 2. Classification (분류)/2_2_1_AI는 체격을 보고 초, 중, 고등학생을 구분할 수 있을까/main.py
@@ -12,19 +12,19 @@ EPOCHS = 20  # 예제 기본값은 20입니다.
 dr = data_reader.DataReader()
 
 # 인공신경망을 제작합니다.
-graph = keras.Sequential([
+model = keras.Sequential([
     keras.layers.Dense(3),
     keras.layers.Dense(128, activation="relu"),
     keras.layers.Dense(3, activation='softmax')
 ])
 
 # 인공신경망을 컴파일합니다.
-graph.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 # 인공신경망을 학습시킵니다.
 print("\n\n************ TRAINING START ************ ")
 early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-history = graph.fit(dr.train_X, dr.train_Y, epochs=EPOCHS, validation_data=(dr.test_X, dr.test_Y), callbacks=[early_stop])
+history = model.fit(dr.train_X, dr.train_Y, epochs=EPOCHS, validation_data=(dr.test_X, dr.test_Y), callbacks=[early_stop])
 
 # 학습 결과를 그래프로 출력합니다.
 data_reader.draw_graph(history)
