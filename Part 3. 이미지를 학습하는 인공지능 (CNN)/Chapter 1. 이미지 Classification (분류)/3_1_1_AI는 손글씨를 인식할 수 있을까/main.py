468ec93ff4433e9d0b2fe111dfe52f672a982a1a
needleworm <bhban@kakao.com>
2020-08-20 16:22:51 +0900

    .

--- Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 1. 이미지 Classification (분류)/3_1_1_AI는 손글씨를 인식할 수 있을까/main.py
+++ Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 1. 이미지 Classification (분류)/3_1_1_AI는 손글씨를 인식할 수 있을까/main.py
@@ -12,21 +12,20 @@ EPOCHS = 20  # 예제 기본값은 20입니다.
 dr = data_reader.DataReader()
 
 # 인공신경망을 제작합니다.
-# 총 3층짜리 신경망입니다.
-graph = keras.Sequential([
+model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
 
 # 인공신경망을 컴파일합니다.
-graph.compile(optimizer='adam', metrics=['accuracy'],
+model.compile(optimizer='adam', metrics=['accuracy'],
               loss='sparse_categorical_crossentropy')
 
 # 인공신경망을 학습시킵니다.
 print("\n\n************ TRAINING START ************ ")
 early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-history = graph.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
+history = model.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
                     validation_data=(dr.test_X, dr.test_Y),
                     callbacks=[early_stop])
 
