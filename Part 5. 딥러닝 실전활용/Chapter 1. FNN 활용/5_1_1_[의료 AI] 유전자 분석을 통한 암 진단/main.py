e380738d4fe25f15ac869582b60aaa3105e3fd86
needleworm <bhban@kakao.com>
2020-08-22 04:41:28 +0900

    .

--- Part 5. 딥러닝 실전활용/Chapter 1. FNN 활용/5_1_1_[의료 AI] 유전자 분석을 통한 암 진단/main.py
+++ Part 5. 딥러닝 실전활용/Chapter 1. FNN 활용/5_1_1_[의료 AI] 유전자 분석을 통한 암 진단/main.py
@@ -5,16 +5,14 @@ Date : 2020.07.24.
 from tensorflow import keras
 import data_reader
 
-
 # 몇 에포크 만큼 학습을 시킬 것인지 결정합니다.
-EPOCHS = 2  # 예제 기본값은 20입니다.
+EPOCHS = 20  # 예제 기본값은 20입니다.
 
 # 데이터를 읽어옵니다.
 dr = data_reader.DataReader()
 
 # 인공신경망을 제작합니다.
-# 총 3층짜리 신경망입니다.
-graph = keras.Sequential([
+model = keras.Sequential([
     keras.layers.Dense(20000),
     keras.layers.Dense(2048, activation="relu"),
     keras.layers.Dropout(rate=0.5),
@@ -24,13 +22,13 @@ graph = keras.Sequential([
 ])
 
 # 인공신경망을 컴파일합니다.
-graph.compile(optimizer="adam", metrics=["accuracy"],
+model.compile(optimizer="adam", metrics=["accuracy"],
               loss="sparse_categorical_crossentropy")
 
 # 인공신경망을 학습시킵니다.
 print("\n\n************ TRAINING START ************ ")
 early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-history = graph.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
+history = model.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
                     validation_data=(dr.test_X, dr.test_Y),
                     callbacks=[early_stop])
 
