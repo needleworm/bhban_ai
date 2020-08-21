f2468de271e602de295838f0ecb834ad24378e3a
needleworm <bhban@kakao.com>
2020-08-22 00:19:30 +0900

    .

--- Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 1. 이미지 Classification (분류)/3_1_4_CNN AI는 사물을 구분할 수 있을까/main.py
+++ Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 1. 이미지 Classification (분류)/3_1_4_CNN AI는 사물을 구분할 수 있을까/main.py
@@ -14,13 +14,17 @@ dr = data_reader.DataReader()
 
 # 인공신경망을 제작합니다.
 graph = keras.Sequential([
-    keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
+    keras.layers.Conv2D(32, (3, 3), activation="relu"),
+    keras.layers.BatchNormalization(),
     keras.layers.MaxPooling2D((2, 2)),
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.BatchNormalization(),
     keras.layers.MaxPooling2D((2, 2)),
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.BatchNormalization(),
     keras.layers.Flatten(),
     keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dropout(rate=0.5),
     keras.layers.Dense(10, activation='softmax')
 ])
 
