42680d1bb7a71fe35af3d4afaca8f522b63e2f86
needleworm <bhban@kakao.com>
2020-08-22 21:08:15 +0900

    .

--- Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 2. 이미지 학습 고급/3_2_1_AI는 거장의 화풍을 따라할 수 있을까 - Style Transfer/main.py
+++ Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 2. 이미지 학습 고급/3_2_1_AI는 거장의 화풍을 따라할 수 있을까 - Style Transfer/main.py
@@ -8,9 +8,10 @@ import data_reader
 # 데이터를 불러옵니다.
 dr = data_reader.DataReader("content.jpg", "style.jpg")
 
-# TF Hub를 통해 작업을 수행합니다.
 # Hub로부터 style transfer 모듈을 불러옵니다.
-hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
+hub_module = hub.load(
+    'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1'
+)
 
 # 모듈에 이미지를 삽입해 Style Transfer를 실시합니다.
 stylized_image = hub_module(dr.content, dr.style)[0]
