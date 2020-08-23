e0fae52167cf6090ab838bea89d15143e0e39f38
needleworm <bhban@kakao.com>
2020-08-23 21:10:08 +0900

    .

--- Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 1. 이미지 Classification (분류)/3_1_4_CNN AI는 사물을 구분할 수 있을까/main.py
+++ Part 3. 이미지를 학습하는 인공지능 (CNN)/Chapter 1. 이미지 Classification (분류)/3_1_4_CNN AI는 사물을 구분할 수 있을까/main.py
@@ -12,15 +12,18 @@ EPOCHS = 20  # 예제 기본값은 20입니다.
 dr = data_reader.DataReader()
 
 # 인공신경망을 제작합니다.
-graph = keras.Sequential([
-    keras.layers.Conv2D(32, (3, 3), activation="relu"),
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3)),
     keras.layers.BatchNormalization(),
+    keras.layers.ReLU(),
     keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.Conv2D(64, (3, 3)),
     keras.layers.BatchNormalization(),
+    keras.layers.ReLU(),
     keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.Conv2D(64, (3, 3)),
     keras.layers.BatchNormalization(),
+    keras.layers.ReLU(),
     keras.layers.Flatten(),
     keras.layers.Dense(128, activation='relu'),
     keras.layers.Dropout(rate=0.5),
@@ -28,13 +31,13 @@ graph = keras.Sequential([
 ])
 
 # 인공신경망을 컴파일합니다.
-graph.compile(optimizer='adam', metrics=['accuracy'],
+model.compile(optimizer='adam', metrics=['accuracy'],
               loss='sparse_categorical_crossentropy')
 
 # 인공신경망을 학습시킵니다.
 print("\n\n************ TRAINING START ************ ")
 early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-history = graph.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
+history = model.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
                     validation_data=(dr.test_X, dr.test_Y),
                     callbacks=[early_stop])
 
