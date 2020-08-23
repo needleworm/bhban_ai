66123f9a5d7ff7a0fdec9c60b9884952d43ed2f9
needleworm <bhban@kakao.com>
2020-08-23 20:27:45 +0900

    .

--- Part 4. 시계열 데이터를 학습하는 인공지능 (LSTM)/Chapter 1. 자연어 학습 인공지능/4_1_1_AI는 스팸 문자를 걸러낼 수 있을까/main.py
+++ Part 4. 시계열 데이터를 학습하는 인공지능 (LSTM)/Chapter 1. 자연어 학습 인공지능/4_1_1_AI는 스팸 문자를 걸러낼 수 있을까/main.py
@@ -5,7 +5,6 @@ Date : 2020.07.24.
 import data_reader
 from tensorflow import keras
 
-
 # 몇 에포크 만큼 학습을 시킬 것인지 결정합니다.
 EPOCHS = 50  # 예제 기본값은 50입니다.
 
@@ -13,7 +12,7 @@ EPOCHS = 50  # 예제 기본값은 50입니다.
 dr = data_reader.DataReader()
 
 # 인공신경망을 제작합니다.
-graph = keras.Sequential([
+model = keras.Sequential([
     keras.layers.Embedding(8983, 128),
     keras.layers.GlobalAveragePooling1D(),
     keras.layers.Dense(32, activation='relu'),
@@ -22,13 +21,13 @@ graph = keras.Sequential([
 ])
 
 # 인공신경망을 컴파일합니다.
-graph.compile(optimizer="adam", metrics=['accuracy']
+model.compile(optimizer="adam", metrics=['accuracy']
               , loss="binary_crossentropy")
 
 # 인공신경망을 학습시킵니다.
 print("\n\n************ TRAINING START ************ ")
 early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-history = graph.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
+history = model.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
                     validation_data=(dr.test_X, dr.test_Y),
                     callbacks=[early_stop])
 
