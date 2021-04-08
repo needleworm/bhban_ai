d0b6711b4784c3702dc8598aa6e481e0aeca0c61
needleworm <bhban@kakao.com>
2021-04-08 14:39:08 +0900

    .

--- 4_11_3_LSTM AI는 얼마나 성능이 뛰어날까/main.py
+++ 4_11_3_LSTM AI는 얼마나 성능이 뛰어날까/main.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 import data_reader
 
 # 몇 에포크 만큼 학습을 시킬 것인지 결정합니다.
-EPOCHS = 50  # 예제 기본값은 50입니다.
+EPOCHS = 1  # 예제 기본값은 1입니다.
 
 # 데이터를 읽어옵니다.
 dr = data_reader.DataReader()
@@ -14,7 +14,7 @@ dr = data_reader.DataReader()
 # 인공신경망을 제작합니다.
 model = keras.Sequential([
     keras.layers.Embedding(8983, 128),
-    keras.layers.GlobalAveragePooling1D(),
+    keras.layers.Bidirectional(keras.layers.LSTM(32)),
     keras.layers.Dense(32, activation='relu'),
     keras.layers.Dropout(0.1),
     keras.layers.Dense(1, activation='sigmoid')
@@ -26,10 +26,8 @@ model.compile(optimizer="adam", metrics=['accuracy'],
 
 # 인공신경망을 학습시킵니다.
 print("\n\n************ TRAINING START ************ ")
-early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 history = model.fit(dr.train_X, dr.train_Y, epochs=EPOCHS,
-                    validation_data=(dr.test_X, dr.test_Y),
-                    callbacks=[early_stop])
+                    validation_data=(dr.test_X, dr.test_Y))
 
 # 학습 결과를 그래프로 출력합니다.
 data_reader.draw_graph(history)
